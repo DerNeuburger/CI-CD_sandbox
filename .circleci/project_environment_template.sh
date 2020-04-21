9a9262880ed76a2392c6f3d96d635287903eabbb
Nicolas Neuburger <ga67bah@mytum.de>
2020-04-21 14:22:27 +0200

    Added shell template for local deployment

---
+++ .circleci/project_environment_template.sh
@@ -0,0 +1,6 @@
+export PROJECT_MAIN_VERSION="<value>"
+export PROJECT_SUB_VERSION="<value>"
+export DOCKER_USER="<value>"
+export DOCKER_PASS="<value>"
+export DOCKER_IMAGE_NAME="<value>"
+export CIRCLE_BUILD_NUM="<value>"
