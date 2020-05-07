22b45bd899dbd1d8d09a0c941246256148371dde
Nicolas Neuburger <ga67bah@mytum.de>
2020-05-07 18:20:52 +0200

    MyIp Address requested when creating bastion stack

--- scripts/run_infrastructure.sh
+++ scripts/run_infrastructure.sh
@@ -6,46 +6,50 @@ iac_source_path="../infrastructure-as-code"
 
 declare -a modules=()
 
-function usage()
-{
-    echo "run_infastructure.sh [Flags]"
-    echo ""
-    echo "./manage_stack.sh"
-    echo "\t-h --help=Provision of help information"
-    echo "\t--update=Updates existing stack(s)"
-    echo ""
+function usage() {
+	echo "run_infastructure.sh [Flags]"
+	echo ""
+	echo "./manage_stack.sh"
+	echo "\t-h --help=Provision of help information"
+	echo "\t--update=Updates existing stack(s)"
+	echo ""
 }
 
 while [[ $1 = -* ]]; do
 
-    PARAM=`echo $1 | awk -F= '{print $1}'`
+	PARAM=$(echo $1 | awk -F= '{print $1}')
 
-    case $PARAM in
-        --module | -m)
-            modules+="$2"
-            ;;
-        -h | --help)
-            usage
-            exit 0
-            ;;
-        -u | --update)
-            update="-u"
-            ;;
-        *)
-            echo "Unknown argument passed to this script."
-            usage
-            exit 1
-            ;;
-    esac
-    shift
+	case $PARAM in
+	--module | -m)
+		modules+="$2"
+		;;
+	-h | --help)
+		usage
+		exit 0
+		;;
+	-u | --update)
+		update="-u"
+		;;
+	*)
+		echo "Unknown argument passed to this script."
+		usage
+		exit 1
+		;;
+	esac
+	shift
 done
 
-if ! (( ${#modules[@]} )); then
-    modules=( "network" "permissions" "webservers" "ansible-master" "bastion-hosts" )
+if ! ((${#modules[@]})); then
+	modules=("network" "permissions" "webservers" "ansible-master" "bastion-hosts")
 fi
 
-for i in "${modules[@]}"
-do
-    echo "./manage_stack.sh -c -w "$ID-$i" ${update} "${iac_source_path}/cfn_$i.yml "${iac_source_path}/parameters_$i.json"
-    ./manage_stack.sh -c -w "$ID-$i" ${update} "${iac_source_path}/cfn_$i.yml" "${iac_source_path}/parameters_$i.json"
+for i in "${modules[@]}"; do
+	echo "./manage_stack.sh -c -w "$ID-$i" ${update} "${iac_source_path}/cfn_$i.yml "${iac_source_path}/parameters_$i.json"
+	parameters+="${iac_source_path}/parameters_$i.json"
+	if [ $i == "bastion-hosts" ]; then
+		read -e -p "Enter your public IPv4 address " userIpV4
+		parameters+=" --parameters MyCidrIpAddress=${userIpV4}"
+	fi
+	parameters+="${iac_source_path}/parameters_$i.json"
+	./manage_stack.sh -c -w "$ID-$i" ${update} "${iac_source_path}/cfn_$i.yml" $parameters
 done
