6bf057ea7671ca778e09775a467593d7137ff9f8
Nicolas Neuburger <ga67bah@mytum.de>
2020-05-16 18:24:55 +0200

    Add delete functionality to startup script

--- scripts/run_infrastructure.sh
+++ scripts/run_infrastructure.sh
@@ -9,10 +9,12 @@ IAM_CAPABILITIES=""
 WAIT_CREATE_COMPLETED=false
 EXTRA_PARAM=""
 IAM_CAPABILITIES="--capabilities CAPABILITY_NAMED_IAM"
+DELETE=false
 update=""
 iac_source_path="../infrastructure-as-code"
 extra_parameters=""
 parameters_replaced=false
+WAIT_COMMAND="stack-create-complete"
 
 declare -a modules=()
 
@@ -30,16 +32,25 @@ while [[ $1 = -* ]]; do
 	PARAM=$(echo $1 | awk -F= '{print $1}')
 
 	case $PARAM in
-	--module | -m)
+	-m | --module)
 		modules+="$2"
+                echo "module $2 given"
+                shift 2
 		;;
 	-h | --help)
 		usage
 		exit 0
 		;;
+	-d | --delete)
+		DELETE=true
+		WAIT_COMMAND="stack-delete-complete"
+                echo "delete given"
+                shift 1
+		;;
 	-u | --update)
 		STACK_COMMAND="update-stack"
 		WAIT_COMMAND="stack-update-complete"
+                echo "update given"
 		;;
 	*)
 		echo "Unknown argument passed to this script."
@@ -47,7 +58,6 @@ while [[ $1 = -* ]]; do
 		exit 1
 		;;
 	esac
-	shift
 done
 
 if ! ((${#modules[@]})); then
@@ -56,7 +66,6 @@ fi
 
 for i in "${modules[@]}"; do
 	STACK_COMMAND="create-stack"
-	WAIT_COMMAND="stack-create-complete"
 	WAIT_CREATE_COMPLETED=false
 	parameter_rel_filepath="${iac_source_path}/parameters_$i.json"
 	parameters="file://${iac_source_path}/parameters_$i.json"
@@ -68,7 +77,14 @@ for i in "${modules[@]}"; do
 		STACK_COMMAND+=" $IAM_CAPABILITIES"
 		WAIT_CREATE_COMPLETED=true
 	fi
-	if [ $i == "bastion-hosts" ]; then
+	
+        if $DELETE; then
+             aws cloudformation delete-stack --stack-name $1-$i
+		if $WAIT_CREATE_COMPLETED; then
+			aws cloudformation wait $WAIT_COMMAND --stack-name $1-$i
+		fi
+        else
+	     if [[ ($i == "bastion-hosts") || ($i == "jenkins-server") ]]; then
 		read -e -p "Enter your public IPv4 address " userIpV4
 		#parameters_replaced=true
 		#parameter_file_template=$parameter_file
@@ -76,11 +92,11 @@ for i in "${modules[@]}"; do
 		#sed -e "s/TEMPLATE_MyCidrIpAddress/${userIpV4}/g" $parameter_file_template > $parameter_file
 		parameters=$(sed -e "s~TEMPLATE_MyCidrIpAddress~${userIpV4}~g" $parameter_rel_filepath | tr '\n' ' ')
 		echo $parameters
-	fi
-	echo "aws cloudformation $STACK_COMMAND --stack-name $i --template-body "file://${iac_source_path}/cfn_$i.yml" --parameters $parameters --region eu-central-1"
-	aws cloudformation $STACK_COMMAND --stack-name $1-$i --template-body "file://${iac_source_path}/cfn_$i.yml" --parameters "$parameters" --region eu-central-1
+	     fi
+             aws cloudformation $STACK_COMMAND --stack-name $1-$i --template-body "file://${iac_source_path}/cfn_$i.yml" --parameters "$parameters" --region eu-central-1
 
-	if $WAIT_CREATE_COMPLETED; then
-		aws cloudformation wait $WAIT_COMMAND --stack-name $1-$i
-	fi
+		if $WAIT_CREATE_COMPLETED; then
+			aws cloudformation wait $WAIT_COMMAND --stack-name $1-$i
+		fi
+        fi
 done
