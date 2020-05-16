14880001b9d6aa7b95b877008571f0de114901ca
Nicolas Neuburger <ga67bah@mytum.de>
2020-05-16 19:48:33 +0200

    Adapt to EnvironmentName as passed Argument

--- scripts/run_infrastructure.sh
+++ scripts/run_infrastructure.sh
@@ -77,24 +77,24 @@ for i in "${modules[@]}"; do
 		STACK_COMMAND+=" $IAM_CAPABILITIES"
 		WAIT_CREATE_COMPLETED=true
 	fi
-	
-        if $DELETE; then
-             aws cloudformation delete-stack --stack-name $1-$i
-	     aws cloudformation wait $WAIT_COMMAND --stack-name $1-$i
-        else
-	     if [[ ($i == "bastion-hosts") || ($i == "jenkins-server") ]]; then
-		read -e -p "Enter your public IPv4 address " userIpV4
-		#parameters_replaced=true
-		#parameter_file_template=$parameter_file
-		#parameter_file="${iac_source_path}/parameters_${i}_filled.json"
-		#sed -e "s/TEMPLATE_MyCidrIpAddress/${userIpV4}/g" $parameter_file_template > $parameter_file
-		parameters=$(sed -e "s~TEMPLATE_MyCidrIpAddress~${userIpV4}~g" $parameter_rel_filepath | tr '\n' ' ')
+
+	if $DELETE; then
+		aws cloudformation delete-stack --stack-name $1-$i
+		aws cloudformation wait $WAIT_COMMAND --stack-name $1-$i
+	else
+		if [[ ($i == "bastion-hosts") || ($i == "jenkins-server") ]]; then
+			read -e -p "Enter your public IPv4 address " userIpV4
+			#parameters_replaced=true
+			#parameter_file_template=$parameter_file
+			#parameter_file="${iac_source_path}/parameters_${i}_filled.json"
+			#sed -e "s/TEMPLATE_MyCidrIpAddress/${userIpV4}/g" $parameter_file_template > $parameter_file
+			parameters=$(echo $parameters | sed -e "s~TEMPLATE_MyCidrIpAddress~${userIpV4}~g")
+		fi
 		echo $parameters
-	     fi
-             aws cloudformation $STACK_COMMAND --stack-name $1-$i --template-body "file://${iac_source_path}/cfn_$i.yml" --parameters "$parameters" --region eu-central-1
+		aws cloudformation $STACK_COMMAND --stack-name $1-$i --template-body "file://${iac_source_path}/cfn_$i.yml" --parameters "$parameters" --region eu-central-1
 
 		if $WAIT_CREATE_COMPLETED; then
 			aws cloudformation wait $WAIT_COMMAND --stack-name $1-$i
 		fi
-        fi
+	fi
 done
